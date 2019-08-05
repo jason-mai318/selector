2e4aa00d0518b941556ac19aee27f7f4a72ce446
Student <jsitbon@qcc.cuny.edu>
2019-08-05 14:47:26 -0400

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Shape from "./shape.js"
 
@@ -8,7 +7,7 @@ class Selector extends Component {
   constructor() {
     super();
     this.state = {
-      selectedShape: 'square',
+      selectedShape: 'none',
     }
   }
 
@@ -25,15 +24,15 @@ class Selector extends Component {
           Selected Shape: <span> {this.state.selectedShape}</span>
         </div>
         <div className="shape-list">
-          <Shape shape="square" selectShape = {this.selectShape}/>
-          <Shape shape="circle" selectShape = {this.selectShape}/>
-          <Shape shape="trapezium" selectShape = {this.selectShape}/>
-          <Shape shape="oval" selectShape = {this.selectShape}/>
-          <Shape shape="star" selectShape = {this.selectShape}/>
-          <Shape shape="triangle_down" selectShape = {this.selectShape}/>
-          <Shape shape="triangle_left" selectShape = {this.selectShape}/>
-          <Shape shape="triangle_right" selectShape = {this.selectShape}/>
-          <Shape shape="triangle" selectShape = {this.selectShape}/>
+          <Shape shape="square" id = "1" selectShape = {this.selectShape}/>
+          <Shape shape="circle" id = "2" selectShape = {this.selectShape}/>
+          {/* <Shape shape="trapezium" id = "3" selectShape = {this.selectShape}/>
+          <Shape shape="oval" id = "4" selectShape = {this.selectShape}/>
+          <Shape shape="star" id = "5" selectShape = {this.selectShape}/>
+          <Shape shape="triangle_down" id = "6" selectShape = {this.selectShape}/>
+          <Shape shape="triangle_left" id = "7" selectShape = {this.selectShape}/>
+          <Shape shape="triangle_right" id = "8" selectShape = {this.selectShape}/>
+          <Shape shape="triangle" id = "9" selectShape = {this.selectShape}/> */}
         </div>
       </div>
     );
--- src/shape.js
+++ src/shape.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Shape = (props) => {
     const shape = props.shape
     const selectShape = props.selectShape
+    const id = props.id
     return(
         <div className={shape} onClick={() => selectShape(shape)}/>
 
